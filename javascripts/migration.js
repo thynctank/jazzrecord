a16f6bb07ef968ceb34016020a4f6d31bdc0ea6d
Nick Carter <thynctank@thynctank.com>
2008-08-25 05:31:35 +0000

    fleshed out much of migrations (more complex migrations to come require temp tables).
    Need to finish writing migration types, create migration table for numbering.
    
    Write docs for migrations.
    
    Need to separate adapters into another include, write specification for adapters.
    
    Need to add validations. Write documentation.
    
    Need to add working associations. Start with has_many/belongs_to and has_one/belongs_to. HABTM next. Write docs for associations.
    
    Need to add Model/Record methods. Define in Model def. Write docs.

--- javascripts/migration.js
+++ javascripts/migration.js
@@ -1,16 +1,54 @@
 ThyncRecord.Migration = {
-  addTable: function() {},
-  removeTable: function() {},
-  addColumn: function() {},
-  removeColumn: function() {},
-  renameColumn: function() {},
-  changeColumn: function() {}
+  createTable: function(name, columns) {
+    if(!($defined(name) && $defined(columns))) {
+      return;
+    }
+    var sql = "CREATE TABLE IF NOT EXISTS " + name;
+    if (columns) {
+      sql += "(";
+      for (col in columns) {
+        sql += (col + " " + columns[col].toUpperCase() + ", ");
+      }
+      sql = sql.substr(0, sql.length - 2);
+      sql += ");";
+      ThyncRecord.adapter.run(sql);
+    }
+  },
+  dropTable: function(name) {
+    var sql = "DROP TABLE " + name + ";";
+    ThyncRecord.adapter.run(sql);
+  },
+  renameTable: function(oldName, newName) {
+    var sql = "ALTER TABLE " + oldName + " RENAME TO " + newName + ";";
+    ThyncRecord.adapter.run(sql);
+  },
+  addColumn: function(tableName, columnName, dataType) {
+    var sql = "ALTER TABLE " + tableName + " ADD COLUMN " + columnName + " " + dataType.toUpperCase() + ";";
+    ThyncRecord.adapter.run(sql);
+  },
+  //last three mods will need temp table copy due to sqlite limitations
+  removeColumn: function(tableName, columnName) {},
+  renameColumn: function(oldName, newName) {},
+  changeColumn: function(tableName, columnName, type) {}
 };
 
-ThyncRecord.migrate = function() {
+ThyncRecord.migrate = function(startIndex) {
   if(this.migrations) {
-    this.migrations.each(function(m, index) {
-    });
+    //tack in code that only runs migrations starting from most recently-run index
+    for( var i = startIndex || 0, j = this.migrations.length; i < j; i++) {
+      m = this.migrations[i];
+      var operation = m[0];
+      switch (m.length) {
+        case 4:
+          ThyncRecord.Migration[operation](m[1], m[2], m[3]);
+          break;
+        case 3:
+          ThyncRecord.Migration[operation](m[1], m[2]);
+          break;
+        case 2:
+          ThyncRecord.Migration[operation](m[1]);
+      }
+    }
   }
   else {
     this.models.each(function(model) {

9a16b2edf6402cad72e785b0dacc826b05652c62
David Rivers <ideogon@gmail.com>
2008-10-03 15:36:49 +0000

    Find now accepts either an ID, an array of IDs, or an object literal specifying parameters to the SQL statement. Accordingly I modified the select method so that options.id can be either of type Number or type Array.

--- javascripts/find.js
+++ javascripts/find.js
@@ -1,17 +1,21 @@
 ThyncRecord.Model.implement({
   //finders: find, find_by, all, first, last
-  find: function(options) {
-			if($type(options)=='number')
-    	options = $extend({id: ids, limit: 1}, options);			
-		else if($type(options)=='string')
-			options = $extend({conditions: " id = " + ids.toString().replace(/,/g, " or id = ")}, options);
-    // if(!$defined(ids))
-    //   throw("Missing ID Parameter");
-    // if($splat(ids).length == 1)
-    //   options = $extend({id: ids, limit: 1}, options);
-    // else
-    //   options = $extend({conditions: " id = " + ids.toString().replace(/,/g, " or id = ")}, options);
-    return this.select(options);
+  find: function(query) {
+			// if($type(options)=='number')
+			// 	options = $extend({id: options});
+			// else if($type(options)=='string')
+			// 	options = $extend({conditions: " id = " + ids.toString().replace(/,/g, " or id = ")}, options);
+			
+    if(!$defined(query))
+      throw("Missing ID or Options Parameters");
+		else
+			if($type(query)=='number' || $type(query)=='array')
+      	options = {id: query};
+	    else if($type(query)=='object')
+	      options = query;
+			else
+				throw("Type Error. You can find by either an ID, an array of IDs, or an object literal containing values for SELECT, TABLE, CONDITIONS, ORDER, LIMIT, and OFFSET.");
+	  return this.select(options);
   },
   findBy: function(field, value) {
     if(!this.options.columns[field])
@@ -67,8 +71,10 @@ ThyncRecord.Model.implement({
         options.conditions += " AND id=" + options.id;
     }
     else if(options.id)
-      options.conditions = "WHERE id=" + options.id;
-      
+			if($type(options.id)=='number')
+      	options.conditions = "WHERE id=" + options.id;
+			else if($type(options.id)=='array')
+      	options.conditions = "WHERE id IN (" + options.id + ")";
     this.sql = this.sql.substitute(options).clean() + ";";
     
     return this.query(options);

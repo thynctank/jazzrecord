0e241295c8b4090340e039ca76d30ae636d53d9a
Nick Carter <thynctank@thynctank.com>
2008-10-03 16:24:58 +0000

    swapped tabs for dblspaces

--- javascripts/find.js
+++ javascripts/find.js
@@ -3,14 +3,14 @@ ThyncRecord.Model.implement({
   find: function(query) {
     if(!$defined(query))
       throw("Missing ID or Options Parameters");
-		else
-			if($type(query)=='number' || $type(query)=='array')
-      	options = {id: query};
-	    else if($type(query)=='object')
-	      options = query;
-			else
-				throw("Type Error. You can find by either an ID, an array of IDs, or an object literal containing values for SELECT, TABLE, CONDITIONS, ORDER, LIMIT, and OFFSET.");
-	  return this.select(options);
+    else
+      if($type(query)=='number' || $type(query)=='array')
+        options = {id: query};
+      else if($type(query)=='object')
+        options = query;
+      else
+        throw("Type Error. You can find by either an ID, an array of IDs, or an object literal containing values for SELECT, TABLE, CONDITIONS, ORDER, LIMIT, and OFFSET.");
+    return this.select(options);
   },
   findBy: function(field, value) {
     if(!this.options.columns[field])
@@ -66,10 +66,10 @@ ThyncRecord.Model.implement({
         options.conditions += " AND id=" + options.id;
     }
     else if(options.id)
-			if($type(options.id)=='number')
-      	options.conditions = "WHERE id=" + options.id;
-			else if($type(options.id)=='array')
-      	options.conditions = "WHERE id IN (" + options.id + ")";
+      if($type(options.id)=='number')
+        options.conditions = "WHERE id=" + options.id;
+      else if($type(options.id)=='array')
+        options.conditions = "WHERE id IN (" + options.id + ")";
     this.sql = this.sql.substitute(options).clean() + ";";
     
     return this.query(options);

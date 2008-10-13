4240598ed064244f502cf26d5167a086270809e4
Jesse Teates <scix@graffiti.net>
2008-10-13 02:18:32 +0000

    added validations.js, no real code yet.

---
+++ javascripts/validations.js
@@ -0,0 +1,34 @@
+ThyncRecord.Record.implement({
+
+  validate: function() {
+     $each(this.options.columns, function(colType, colName) {
+        switch(colType) {
+	case "text":
+		
+	case "number":
+		
+	case "int":
+		
+	case "float":
+		
+	case "bool":
+		
+	case null:
+	   throw "Invalid datatype passed to validate";
+	
+        }
+     });
+  },
+  
+  isValid: function() {
+    if (this.errors.length != 0)
+       return false;
+    else
+       return true;
+  },
+  
+  validateOnCreate: function() {},
+  
+  validateonSave: function() {}
+  
+});

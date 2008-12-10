9f0c6fbf4c21581f9f11d5215528d5f4f76da2ba
Nick Carter <thynctank@thynctank.com>
2008-12-10 16:19:31 -0500

    added new AIR dependencies. Fixed usage in AIR

--- javascripts/dependencies/air/AIRSourceViewer.js
+++ javascripts/dependencies/air/AIRSourceViewer.js
@@ -1,31 +1,14 @@
-/* AIRSourceViewer.js - Revision: 1.1 */
+/* AIRSourceViewer.js - Revision: 1.5 */
 
-// Copyright 2007-2008 Adobe Systems Incorporated.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-//   * Redistributions of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation
-//     and/or other materials provided with the distribution.
-//   * Neither the name of Adobe Systems Incorporated nor the names of its
-//     contributors may be used to endorse or promote products derived from this
-//     software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
+/*
+ADOBE SYSTEMS INCORPORATED
+Copyright 2007-2008 Adobe Systems Incorporated. All Rights Reserved.
+ 
+NOTICE:   Adobe permits you to modify and distribute this file only in accordance with
+the terms of Adobe AIR SDK license agreement.  You may have received this file from a
+source other than Adobe.  Nonetheless, you may modify or
+distribute this file only in accordance with such agreement. 
+*/
 
 // @the Source Viewer uses the <air> namespace:
 if (typeof air == 'undefined') {air = {}};
@@ -133,7 +116,7 @@ air.SourceViewer.getDefault = function() {
 		var AIR_TOKEN				   = 'AIR';
 		var TRADEMARK_TOKEN			   = '&trade;';
 		var APP_LEGAL_NAME             = 'HTML View Source Framework';
-		var APP_VERSION_MAJOR_MINOR	   = '1.1';
+		var APP_VERSION_MAJOR_MINOR	   = '1.5';
 		var TREE_DESCRIPTION_MESSAGE   = 'Select a source file in the tree to '+
 			                             'see its content in the right pane:';
 		var COPYRIGHT_MESSAGE          = '';
@@ -739,6 +722,11 @@ air.SourceViewer.getDefault = function() {
 						domInitHandler);
 				}
 				htmlLoader.addEventListener('htmlDOMInitialize',domInitHandler);
+				try{
+		            		//since AIR1.5 the htmlLoader will not allow string load in app sandbox
+		            		//surrounded with try/catch in case we are running in older runtime
+		            		htmlLoader.placeLoadStringContentInApplicationSandbox= true;
+	            		}catch(e){}
 				htmlLoader.loadString('');
 			}
 			
